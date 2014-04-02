2353f2e12c700c0d6d7e720b661bdb8a45e34009
Nonlone <thunderbir.shun@gmail.com>
2014-04-02 17:22:25 +0800

    正常模式的列配置器

---
+++ src/com/magdou/seahold/config/colunm/NormalConfig.java
@@ -0,0 +1,14 @@
+package com.magdou.seahold.config.colunm;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NormalConfig {
+	
+	public static Map<String,Field> getColunmConfig(Class<?> clazz){
+		Map<String,Method> methodMap = new HashMap<String,Method>();
+		
+	}
+}
