695b25e2430466e3d8f1efd4dcb3b5e355a4e4a2
Nonlone <thunderbir.shun@gmail.com>
2014-04-03 18:13:00 +0800

    写入获取colunm方法

--- src/com/magdou/seahold/config/colunm/NormalConfig.java
+++ src/com/magdou/seahold/config/colunm/NormalConfig.java
@@ -5,10 +5,33 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.magdou.seahold.utils.StringUtils;
+
 public class NormalConfig {
 	
-	public static Map<String,Field> getColunmConfig(Class<?> clazz){
+	public static Map<String,Field> getColunmConfig(Class<?> classOfT){
 		Map<String,Method> methodMap = new HashMap<String,Method>();
+		Map<String,Field> fieldMap = new HashMap<String,Field>();
+		Class<?> clazz = classOfT;
+		//递归获得类方法
+		while(clazz!=Object.class){
+			for(Method method:classOfT.getDeclaredMethods()){
+				if(!methodMap.containsKey(method.getName())){
+					methodMap.put(method.getName(),method);
+				}
+			}
+			clazz=classOfT.getClass().getSuperclass();
+		}
+		clazz = classOfT;
+		// 递归获得类属性
+		while(clazz!=Object.class){
+			for(Field field:classOfT.getDeclaredFields()){
+				if(!fieldMap.containsKey(field.getName())){
+					fieldMap.put(field.getName(),field);
+				}
+			}
+			clazz = clazz.getClass().getSuperclass();
+		}
 		
 	}
 }
